67eb64a7984e2ee6feb7e58cd18bf508321b58c4
Yılmaz Edis <ylmazedis@gmail.com>
2023-09-13 10:30:57 +0300

    Initial Commit

---
+++ TrioCarouselExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrioCarouselExample
+//
+//  Created by Yilmaz Edis on 13.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
